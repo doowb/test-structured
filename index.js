9a6646ead8d765e3c5b1d61045ae5185cfcc2654
Brian Woodward <brian.woodward@gmail.com>
2015-05-07 14:33:54 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * test-structured <https://github.com/doowb/test-structured>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
